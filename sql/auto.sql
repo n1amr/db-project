551c804587d6db7172087cd58d897d44e0845495
Amr Alaa <n1amr1@gmail.com>
2016-12-07 22:35:07 +0200

    users

---
+++ sql/auto.sql
@@ -0,0 +1,8 @@
+use ecommerce;
+
+source clean.sql;
+source tables.sql;
+source insert-user.sql;
+source insert-user-phone.sql;
+source insert-customer.sql;
+source insert-supplier.sql;
---
+++ sql/insert-supplier.sql
@@ -0,0 +1,15 @@
+USE ecommerce;
+
+INSERT INTO Supplier (UserID,CompanyName)
+VALUES
+  (1,"Aliquet Company"),
+  (10,"Tincidunt Consulting"),
+  (11,"Aliquet Libero Integer Incorporated"),
+  (2,"Elit Etiam Laoreet Associates"),
+  (3,"Posuere Cubilia Curae; Industries"),
+  (4,"A Nunc Limited"),
+  (5,"Duis Risus Institute"),
+  (6,"Egestas Lacinia Foundation"),
+  (7,"Mollis Vitae Posuere Corp."),
+  (8,"Nec Ligula Limited"),
+  (9,"Nisi Corporation");
