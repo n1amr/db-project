92f7bbeafa73bb7227f7a4da2432d1ae5ac7de45
Amr Alaa <n1amr1@gmail.com>
2016-12-07 21:49:03 +0200

    all tables

--- sql/tables.sql
+++ sql/tables.sql
@@ -14,13 +14,76 @@ CREATE INDEX X_User_Email
   ON User (Email);
 
 
-CREATE CREATE TABLE UserPhone (
+CREATE TABLE UserPhone (
   PhoneNumber VARCHAR(20) PRIMARY KEY NOT NULL,
-  UserID      INTEGER,
-  FOREIGN KEY (UserID) REFERENCES User (id)
+  UserID      INTEGER REFERENCES User (ID)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
-CREATE INDEX X_UserPhone_PnoneNumber
+CREATE INDEX X_UserPhone_PhoneNumber
   ON UserPhone (PhoneNumber);
+
+CREATE TABLE Customer (
+  UserID  INTEGER PRIMARY KEY NOT NULL REFERENCES User (ID)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  Balance DECIMAL(10, 2)
+);
+
+CREATE TABLE Supplier (
+  UserID      INTEGER PRIMARY KEY NOT NULL REFERENCES User (ID)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CompanyName VARCHAR(256)
+);
+
+CREATE TABLE Product (
+  ID                 INTEGER PRIMARY KEY NOT NULL,
+  Name               VARCHAR(256),
+  Description        VARCHAR(4096),
+  Price              DECIMAL(8, 2),
+  AvailableQuantitiy INTEGER,
+  SupplierID         INTEGER REFERENCES Supplier (UserID)
+    ON DELETE RESTRICT
+    ON
+    UPDATE CASCADE
+);
+
+CREATE TABLE Orders (
+  ID              INTEGER PRIMARY KEY NOT NULL,
+  UserID          INTEGER REFERENCES User (ID)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  OrderDate       DATETIME,
+  Status          VARCHAR(10),
+  ShippingName    VARCHAR(256),
+  ShippingAddress VARCHAR(2048),
+  ShippingPhone   VARCHAR(20)
+);
+
+CREATE TABLE OrderItem (
+  OrderID  INTEGER NOT NULL REFERENCES Orders (ID)
+    ON DELETE
+    CASCADE
+    ON UPDATE CASCADE,
+  UserID   INTEGER NOT NULL REFERENCES User (ID)
+    ON DELETE
+    CASCADE
+    ON UPDATE CASCADE,
+  Quantity INTEGER,
+  PRIMARY KEY (OrderID, UserID)
+);
+
+CREATE TABLE CartItem (
+  OrderID  INTEGER NOT NULL REFERENCES Orders (ID)
+    ON DELETE
+    CASCADE
+    ON UPDATE CASCADE,
+  UserID   INTEGER NOT NULL REFERENCES User (ID)
+    ON DELETE
+    CASCADE
+    ON UPDATE CASCADE,
+  Quantity INTEGER,
+  PRIMARY KEY (OrderID, UserID)
+);
